ef38ef17bdad2e1e74a303767ac7af51264fa1f7
davidretler <davidretler@gmail.com>
2015-10-18 12:46:34 -0400

    First commit

---
+++ src/Sudoku/Solver/SudokuBoard.java
@@ -0,0 +1,192 @@
+package Sudoku.Solver;
+
+public class SudokuBoard {
+
+	int[][] board;
+
+	public SudokuBoard() {
+		board = new int[9][9];
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				board[i][j] = 0;
+			}
+		}
+	}
+
+	public SudokuBoard(int[][] init) {
+		board = init;
+	}
+
+	/**
+	 * Sets position (i,j) to num if valid position. Returns true if valid.
+	 * Returns false if invalid, and does not allow change to be made
+	 */
+	public boolean set(int i, int j, int num) {
+		if (num < 0 || num > 9)
+			throw new IllegalArgumentException();
+		if (board[i][j] != 0)
+			throw new RuntimeException();
+
+		int oldIJ = board[i][j];
+		board[i][j] = num;
+
+		if (this.check()) {
+			return true;
+		} else {
+			board[i][j] = oldIJ;
+			return false;
+		}
+	}
+
+	/**
+	 * Unsets the board positon (i,j)
+	 */
+	public void unSet(int i, int j) {
+		board[i][j] = 0;
+	}
+
+	public boolean check() {
+		//check the rows
+		for(int row = 0; row < 9; row++) {
+			boolean[] used = {false, false, false, false, false, false, false, false, false};
+			
+			//check along each row
+			for(int j = 0; j < 9; j++) {
+				if(board[row][j] != 0) {
+					if(used[board[row][j] - 1]) return false;
+					used[board[row][j] - 1] = true;
+				}
+			}
+		}
+		
+		//check the cols
+		for(int col = 0; col < 9; col++) {
+			boolean[] used = {false, false, false, false, false, false, false, false, false};
+			
+			//check down each col
+			for(int i = 0; i < 9; i++) {
+				if(board[i][col] != 0) {
+					if(used[board[i][col] - 1]) return false;
+					used[board[i][col] - 1] = true;
+				}
+			}
+		}
+		
+		//check the squares
+		for(int xOff = 0; xOff < 9; xOff += 3) {
+			for(int yOff = 0; yOff < 9; yOff += 3) {
+				boolean[] used = {false, false, false, false, false, false, false, false, false};
+				
+				for(int i = 0; i < 3; i++) {
+					for(int j = 0; j < 3; j++) {
+						if(board[xOff + i][yOff + j] != 0) {
+							if(used[board[xOff + i][yOff + j] - 1]) return false;
+							used[board[xOff + i][yOff +j] - 1] = true;
+						}
+					}
+				}
+			}
+		}
+		
+		return true;
+	}
+
+	public String toString() {
+		// really ugly code for formatting the board
+		String out = "------- ------- -------\n";
+
+		for (int i = 0; i < 9; i++) {
+			out += '|';
+			for (int j = 0; j < 9; j++) {
+				out += board[i][j] == 0 ? " " : board[i][j];
+				out += (j + 1) % 3 == 0 ? (j != 8 ? "|  |" : "| ") : "|";
+			}
+			out += "\n-------  -------  -------\n";
+			if ((i + 1) % 3 == 0) {
+				if (i != 8)
+					out += "\n-------  -------  -------\n";
+				else
+					out += "\n";
+			}
+
+		}
+
+		return out;
+	}
+
+	/**
+	 * Tells us whether we have a move that definitely must be made for (i,j)
+	 * Returns the value (i,j) must take. 0 if undecidable
+	 */
+	public void definiteMove(int i, int j) {
+
+		// only try to make a move if this spot hasn't been set already
+		if (board[i][j] != 0)
+			return;
+
+		int count = 0;
+
+		for (int num = 1; num <= 9; num++) {
+			if (this.set(i, j, num)) {
+				count++;
+				this.unSet(i, j);
+			}
+		}
+
+		// if only one possible move, make that move
+		if (count == 1) {
+			for (int num = 1; num <= 9; num++) {
+				if (this.set(i, j, num)) {
+					break;
+				}
+			}
+		}
+
+	}
+
+	public int getNum(int i, int j) {
+		return board[i][j];
+	}
+
+	public int[][] getState() {
+		int[][] state = new int[9][9];
+
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				state[i][j] = board[i][j];
+			}
+		}
+
+		return state;
+	}
+
+	public void setState(int[][] state) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				board[i][j] = state[i][j];
+			}
+		}
+	}
+
+	/**
+	 * Returns whether the board is solved
+	 * 
+	 * @return
+	 */
+	public boolean solved() {
+		if (this.check()) {
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					if (board[i][j] == 0) {
+						return false;
+					}
+				}
+			}
+			// valid config and all spots filled: yay!
+			return true;
+		}
+		
+		return false;
+	}
+}
---
+++ src/Sudoku/Solver/SudokuTester.java
@@ -0,0 +1,128 @@
+package Sudoku.Solver;
+
+public class SudokuTester {
+
+	static int calls = 0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[][] initial = new int[][]
+		{
+		{ 0, 0, 0, 0, 9, 0, 0, 0, 4 },
+		{ 4, 1, 0, 0, 0, 3, 0, 0, 0 },
+		{ 8, 0, 7, 6, 0, 4, 2, 1, 0 },
+		{ 0, 0, 1, 0, 0, 7, 0, 0, 2 },
+		{ 0, 6, 0, 0, 4, 0, 0, 9, 0 },
+		{ 2, 0, 0, 5, 0, 0, 7, 0, 0 },
+		{ 0, 4, 8, 3, 0, 6, 9, 0, 7 },
+		{ 0, 0, 0, 4, 0, 0, 0, 2, 1 },
+		{ 6, 0, 0, 0, 1, 0, 0, 0, 0 } };
+
+		SudokuBoard myBoard = new SudokuBoard(/*initial*/);
+
+		System.out.println(myBoard);
+		try {
+			if (solve(myBoard)) {
+				System.out.println("\nSolved!\n" + myBoard);
+			} else {
+				System.out.println("Could not solve board...\n"+ myBoard);
+			}
+		} catch (StackOverflowError e) {
+			System.out.println("\nOveflow while solving. Partial output:\n"
+					+ myBoard);
+		}
+
+	}
+
+	/**
+	 * Solves a sudoku board given initial configuration. Returns false if no
+	 * solution exists
+	 * 
+	 * @param board
+	 * @return
+	 */
+	public static boolean solve(SudokuBoard board) {
+		if (!board.check())
+			return false;
+
+		return solve(board, 0, 0);
+	}
+
+	/**
+	 * Recursively solves sudoku board using backtracking Starts at (i,j)
+	 * Returns 0 if no solution
+	 * 
+	 * @return
+	 */
+	private static boolean solve(SudokuBoard board, int i, int j) {
+		if (!board.check())
+			return false;
+
+		if(board.solved()) {
+			return true;
+		}
+		
+		// make sure to save state before making moves
+		int[][] oldState = board.getState();
+		
+		makeDefiniteMoves(board);
+
+		if (board.getNum(i, j) == 0) {
+			// try each number for current configuration, if not already solved
+			for (int n = 1; n <= 9; n++) {
+				if (board.set(i, j, n)) {
+					// if we can set the current position to, do so and solve
+					// rest
+					// of board
+					// next i value is either i+1 or 1 if i is already 9
+					int nextI = i < 8 ? i + 1 : 1;
+					int nextJ = j;
+					if (nextI < i) {
+						// only change the j if i has moved back to 1
+						nextJ = j < 8 ? j + 1 : 1;
+
+					}
+					if (solve(board, nextI, nextJ)) {
+						// if rest of board can be solved from here, backtrack
+						return true;
+					} else {
+						// rest of board cannot be solved so undo the current
+						// change, and restart loop
+						board.unSet(i, j);
+					}
+				}
+			}
+		} else {
+			int nextI = i < 8 ? i + 1 : 1;
+			int nextJ = j;
+			if (nextI < i) {
+				// only change the j if i has moved back to 1
+				nextJ = j < 8 ? j + 1 : 1;
+
+			}
+			if (solve(board, nextI, nextJ)) {
+				// if rest of board can be solved from here, backtrack
+				return true;
+			}
+		}
+
+		//restore state and then return (backtrack)
+		board.setState(oldState); 
+		return false;
+	}
+
+	/**
+	 * Makes moves to the board that we can be certain must be made
+	 */
+	private static void makeDefiniteMoves(SudokuBoard board) {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (board.getNum(i, j) == 0) {
+					board.definiteMove(i, j);
+				}
+			}
+		}
+	}
+
+}
